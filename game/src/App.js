849a4801039c102bb47f46243055e1d9991d583a
veronika-1188 <veronikah747@gmail.com>
2026-01-16 14:59:37 +0500

    itog friday

--- game/src/App.js
+++ game/src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import { useState } from 'react';
+
+const cardImages = [
+    {src: "../public/img/helmet-1.png"},
+    {src: "../public/img/cure-1.png"},
+    {src: "../public/img/dagger-1.png"},
+    {src: "../public/img/potion-1.png"},
+    {src: "../public/img/ring-1.png"},
+    {src: "../public/img/shield-1.png"},
+
+]
 
 function App() {
+    const [cards, setCards] = useState([])
+    const [turns, setTurns] = useState(0)
+
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages, ...cardImages]
+        .sort(() => Math.random() - 0.5)
+        .map((card) => ({...card, id: Math.random()}))
+    }
+
   return (
     <div className="App">
         <h1>Игра "Найди пару"</h1>
